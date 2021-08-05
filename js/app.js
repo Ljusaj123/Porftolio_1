1b3af47dbfb08dd8817a49d7caacdeb40dd4d7e7
Ljusaj123 <rosana.ljusaj2@gmail.com>
2021-08-05 20:23:14 +0200

    podesavanje boja i linkova

--- js/app.js
+++ js/app.js
@@ -9,24 +9,34 @@ menu.addEventListener('click', () => {
 
 const elem = document.querySelector('.highlight');
 const homeMenu = document.querySelector('#home-page');
+const expMenu = document.querySelector('#exp-page');
 const projectsMenu = document.querySelector('#projects-page');
 const contactMenu = document.querySelector('#contact-page');
 
 const highlightMenu = () => {
     if (window.innerWidth > 650 && window.scrollY < 700) {
         homeMenu.classList.add('highlight');
+        expMenu.classList.remove('highlight');
         projectsMenu.classList.remove('highlight');
         contactMenu.classList.remove('highlight');
 
 
     } else if (window.innerWidth > 650 && window.scrollY < 1400) {
         homeMenu.classList.remove('highlight');
-        projectsMenu.classList.add('highlight');
+        expMenu.classList.add('highlight');
+        projectsMenu.classList.remove('highlight');
         contactMenu.classList.remove('highlight');
 
 
-    } else if (window.innerWidth > 650 && window.scrollY < 2345) {
+    } else if (window.innerWidth > 650 && window.scrollY < 2500) {
         homeMenu.classList.remove('highlight');
+        expMenu.classList.remove('highlight');
+        projectsMenu.classList.add('highlight');
+        contactMenu.classList.remove('highlight');
+
+    } else if (window.innerWidth > 650 && window.scrollY < 3300) {
+        homeMenu.classList.remove('highlight');
+        expMenu.classList.remove('highlight');
         projectsMenu.classList.remove('highlight');
         contactMenu.classList.add('highlight');
     }
